1084af0597310473ecead60c80605f13c0a2e679
xiaokang pu <pxk@seiue.com>
2018-02-12 17:36:08 +0800

    basic folder

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+
+/**
+ * TinyVue class
+ * @class
+ * @param {Object} options
+ * @param {HTMLElement} options.el - container box
+ * @param {Object} options.data - props data
+ * @param {HTMLElement} options.template - template
+ */
+var TinyVue = function(options) {
+  console.log(options)
+}
+
+module.exports = TinyVue
