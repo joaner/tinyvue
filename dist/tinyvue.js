9bda5669484d8803cc9a2c12ac57b6c8d0a11efb
xiaokang pu <pxk@seiue.com>
2018-02-13 00:22:46 +0800

    refactor parse attribute

--- dist/tinyvue.js
+++ dist/tinyvue.js
@@ -20,6 +20,12 @@ var TinyVue = function(options) {
   // 解析模板并建立订阅关系
   this.parse = new TinyVueParse(this)
   this.parse.execute()
+
+  // 装载到容器并显示
+  while (this.el.firstChild) {
+    this.el.removeChild(this.el.firstChild);
+  }
+  this.el.appendChild(this.template)
 }
 
 module.exports = TinyVue
@@ -39,13 +45,9 @@ var TinyVueParse = function(vue) {
  * 解析模板并渲染显示
  */
 TinyVueParse.prototype.execute = function() {
-  this.parseNode(this.vue.template)
-
-  // reset render
-  while (this.vue.el.firstChild) {
-    this.vue.el.removeChild(this.vue.el.firstChild);
+  for (var i = 0; i < this.vue.template.childNodes.length; i++) {
+    this.parseElement(this.vue.template.childNodes[i])
   }
-  this.vue.el.appendChild(this.vue.template)
 }
 
 /**
@@ -59,60 +61,20 @@ TinyVueParse.prototype.execExpression = function(expression) {
 
 /**
  * 递归解析DOM节点
+ * @param {HTMLElement} node - 标签节点
  */
-TinyVueParse.prototype.parseNode = function(node) {
+TinyVueParse.prototype.parseElement = function(node) {
+  // 只需要处理标签节点
+  if (node.nodeType !== Node.ELEMENT_NODE) {
+    return
+  }
+
   var self = this
 
   // 解析属性表达式
-  if (node.nodeType === Node.ELEMENT_NODE && node.hasAttributes()) {
+  if (node.hasAttributes()) {
     for (var i = 0; i < node.attributes.length; i++) {
-      var attribute = node.attributes[i]
-      // 只处理 v-* 开头的属性
-      if (attribute.name.indexOf('v-') !== 0) {
-        continue
-      }
-
-      // 指令名
-      var directive = attribute.name.slice(2)
-
-      // v-if里要替换的emptyNode
-      var emptyNode = new Comment()
-
-      // 每次属性变化，都要执行的订阅方法
-      self.vue.proxy.currentDep = (function(directive, attribute, emptyNode) {
-        return function() {
-          switch (directive) {
-            case 'if':
-              var result = self.execExpression(attribute.value)
-              if (!result) {
-                if (node.parentNode) {
-                  node.parentNode.replaceChild(emptyNode, node)
-                }
-              } else {
-                if (emptyNode.parentNode) {
-                  emptyNode.parentNode.replaceChild(node, emptyNode)
-                }
-              }
-              break
-            case 'show':
-              var result = self.execExpression(attribute.value)
-              node.style.display = result ? null : 'none'
-              break
-            case 'click':
-              node.addEventListener(directive, function() {
-                self.execExpression(attribute.value)
-              })
-              break
-          }
-        }
-      })(directive, attribute, emptyNode);
-
-      // 初始执行一次表达式，通过读取属性，来建立订阅关系
-      // 参见 proxy.js 的 Object,defineProperty get方法
-      self.vue.proxy.currentDep()
-
-      // 关系建立完后重置
-      self.vue.proxy.currentDep = null
+      this.parseAttribute(node.attributes[i], node)
     }
   }
 
@@ -120,28 +82,81 @@ TinyVueParse.prototype.parseNode = function(node) {
     for (var i = 0; i < node.childNodes.length; i++) {
       var childNode = node.childNodes[i]
 
-      // 如果是标签节点
-      if (childNode.nodeType === Node.ELEMENT_NODE) {
-        // 此标签的内容为表达式
-        // TODO 不支持解析 {{ expression }} 这样的表达式，因为和其它文本内容混合不好拆分
-        if (childNode.nodeName === 'EXPRESSION') {
-          self.vue.proxy.currentDep = (function(expression, childNode) {
-            return function() {
-              // 每次计算表达式，
-              childNode.textContent = self.execExpression(expression)
-            }
-          })(childNode.textContent, childNode)
+      // 此标签的内容为表达式
+      // TODO 不支持解析 {{ expression }} 这样的表达式，因为和其它文本内容混合不好拆分
+      if (childNode.nodeName === 'EXPRESSION') {
+        self.vue.proxy.currentDep = (function(expression, childNode) {
+          return function() {
+            // 每次计算表达式，
+            childNode.textContent = self.execExpression(expression)
+          }
+        })(childNode.textContent, childNode)
 
-          self.vue.proxy.currentDep()
+        self.vue.proxy.currentDep()
 
-          self.vue.proxy.currentDep = null
-        } else {
-          // 递归解析子标签节点
-          this.parseNode(childNode)
-        }
+        self.vue.proxy.currentDep = null
+      } else {
+        // 递归解析子标签节点
+        this.parseElement(childNode)
       }
     }
   }
+}
+
+/**
+ * 解析属性上的表达式
+ * @param {Attr} attribute - 属性节点
+ * @param {HTMLElement} node - 所属标签节点
+ */
+TinyVueParse.prototype.parseAttribute = function(attribute, node) {
+  // 只处理 v-* 开头的属性
+  if (attribute.name.indexOf('v-') !== 0) {
+    return
+  }
+
+  var self = this
+
+  // 指令名
+  var directive = attribute.name.slice(2)
+
+  // v-if里要替换的emptyNode
+  var emptyNode = new Comment()
+
+  // 每次属性变化，都要执行的订阅方法
+  self.vue.proxy.currentDep = (function(emptyNode) {
+    return function() {
+      switch (directive) {
+        case 'if':
+          var result = self.execExpression(attribute.value)
+          if (!result) {
+            if (node.parentNode) {
+              node.parentNode.replaceChild(emptyNode, node)
+            }
+          } else {
+            if (emptyNode.parentNode) {
+              emptyNode.parentNode.replaceChild(node, emptyNode)
+            }
+          }
+          break
+        case 'show':
+          var result = self.execExpression(attribute.value)
+          node.style.display = result ? null : 'none'
+          break
+        case 'click':
+          node.addEventListener(directive, function() {
+            self.execExpression(attribute.value)
+          })
+          break
+      }
+    }
+  })(emptyNode);
+
+  // 初始执行一次表达式，通过读取属性，来建立订阅关系
+  // 参见 proxy.js 的 Object,defineProperty get方法
+  self.vue.proxy.currentDep()
+
+  // 关系建立完后重置
+  self.vue.proxy.currentDep = null
 }
 
 module.exports = TinyVueParse
--- src/index.js
+++ src/index.js
@@ -19,6 +19,12 @@ var TinyVue = function(options) {
   // 解析模板并建立订阅关系
   this.parse = new TinyVueParse(this)
   this.parse.execute()
+
+  // 装载到容器并显示
+  while (this.el.firstChild) {
+    this.el.removeChild(this.el.firstChild);
+  }
+  this.el.appendChild(this.template)
 }
 
 module.exports = TinyVue
